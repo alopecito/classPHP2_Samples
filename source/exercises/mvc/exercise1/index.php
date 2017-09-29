8f0a1d35fb8680997c1c2dd20717ec70d488772b
Tirso A Lopez <t_opez@concordia.ca>
2017-09-28 21:38:05 -0400

    Solution 3 to the MVP

--- source/exercises/mvc/exercise1/index.php
+++ source/exercises/mvc/exercise1/index.php
@@ -7,5 +7,16 @@ require_once __DIR__.'/controller.php';
 $model = new Model();
 $controller = new Controller($model);
 $view = new View($model);
+$action = $_GET['action'];
+
+switch ($action) {
+	case 'clicked':
+		$controller->click();
+		break;
+	
+	default:
+		
+		break;
+}
 
 echo $view->output();
